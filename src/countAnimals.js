2bcd7daef36b8a54195bb48ec3e050bbb470e92a
lkotlarenko <lkotlarenko@protonmail.com>
2021-11-24 18:48:02 -0300

    "countAnimals" working with no parameters

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  // se não tiver parametro retorna um objeto com chaves (nome da especie: numero de animais da mesma)
+  if (!animal) {
+    return species.reduce((i, spe) => Object.assign(i, { [spe.name]: spe.residents.length }), {});
+  }
 }
 
+console.log(countAnimals());
 module.exports = countAnimals;
