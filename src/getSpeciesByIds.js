107b76b4a5e588c92eba83da4d85730e518f378e
lkotlarenko <lkotlarenko@protonmail.com>
2021-11-24 15:47:27 -0300

    getSpeciesByIds done

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+// lint contra uma linha ;-;
+// const getSpeciesByIds = (...ids) => ids.map((id) => data.species.find((specie) => specie.id.includes(id)));
+
+// vai o outro metodo ent :P
+function getSpeciesByIds (...ids) {
+  return species.filter((specie) => ids.includes(specie.id));
 }
 
 module.exports = getSpeciesByIds;
