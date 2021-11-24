86c09b6922ee963c764b9360ccf170b1e6722c95
lkotlarenko <lkotlarenko@protonmail.com>
2021-11-24 16:28:30 -0300

    getAnimalsOlderThan done

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,13 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// versão uma linha que o lint é contra ;-;
+// const getAnimalsOlderThan = (animal, age) => species.find((specie) => specie.name === animal).residents.every((yOld) => yOld.age >= age);
+
 function getAnimalsOlderThan(animal, age) {
-  const getSpecies = species.find((specie) => specie.name === animal);
+  return species
+    .find((specie) => specie.name === animal)
+    .residents.every((yOld) => yOld.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
