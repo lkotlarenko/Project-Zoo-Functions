2e1388f3cba9e728d1336dc9d43d5ab4785d8370
lkotlarenko <lkotlarenko@protonmail.com>
2021-11-24 19:10:36 -0300

    "countAnimals" done :rocket:

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -6,7 +6,15 @@ function countAnimals(animal) {
   if (!animal) {
     return species.reduce((i, spe) => Object.assign(i, { [spe.name]: spe.residents.length }), {});
   }
+
+  // pega a lista de residentes da especie passada no parametro usando 'find'
+  const resList = species.find((spe) => spe.name === animal.specie).residents;
+
+  // retorna o numero de residentes totais da especie caso o parametro sex nao esteja presente
+  if (!animal.sex) return resList.length;
+
+  // retorna o numero de residentes filtrando(com o parametro sexo) a lista criada anteriormente com o 'filter'
+  return resList.filter((res) => res.sex === animal.sex).length;
 }
 
-console.log(countAnimals());
 module.exports = countAnimals;
