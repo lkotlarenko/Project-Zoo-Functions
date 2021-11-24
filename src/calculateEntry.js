d6177611b54142f0bba2a52302edb9f1abaabad3
lkotlarenko <lkotlarenko@protonmail.com>
2021-11-24 19:58:40 -0300

    "calculateEntry" finished

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -5,12 +5,22 @@ function countEntrants(entrants) {
   const senior = entrants.filter((ent) => ent.age >= 50).length;
   const adult = entrants.filter((ent) => ent.age >= 18 && ent.age < 50).length;
   const child = entrants.filter((ent) => ent.age < 18).length;
+
   // retorna um objeto com chaves contendo o numero de visitantes por idade
   return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  // retorna 0 caso o parametro 'entrants' não seja passado ou seja um objeto vazio;
+  if (!entrants || entrants.length === undefined) return 0;
+
+  // utiliza a funcao acima para calcular as entradas com base nas idades;
+  const senior = countEntrants(entrants).senior * 24.99;
+  const adult = countEntrants(entrants).adult * 49.99;
+  const child = countEntrants(entrants).child * 20.99;
+
+  // devolve o total das entradas;
+  return senior + adult + child;
 }
 
 module.exports = { calculateEntry, countEntrants };
