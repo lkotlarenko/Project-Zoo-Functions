a5d2c6d1047b609ed004479fe70c32b0108e1e1b
lkotlarenko <lkotlarenko@protonmail.com>
2021-11-24 19:27:27 -0300

    "countEntrants" done :cactus:

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,16 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  // usando 'filter' para guardar numero visitantes de diferentes idades
+  const senior = entrants.filter((ent) => ent.age >= 50).length;
+  const adult = entrants.filter((ent) => ent.age >= 18 && ent.age < 50).length;
+  const child = entrants.filter((ent) => ent.age < 18).length;
+  // retorna um objeto com chaves contendo o numero de visitantes por idade
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
-  //começando
 }
 
 module.exports = { calculateEntry, countEntrants };
