b283f6b2c193fd0951a7f6b07dc0fc2fb8b7a33c
lkotlarenko <lkotlarenko@protonmail.com>
2021-11-25 16:21:44 -0300

    "getOldestFromFirstSpecies" done

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  // guarda o id do primeiro animal abaixo da pessoa com o id parametro
+  const ani = employees.find((emp) => emp.id === id).responsibleFor[0];
+  // guarda as variaveis da seguinte maneira: encontra o animal usando o 'find' no id guardado anteriormente,
+  // ordena por idade do mais velho com 'sort' (salva apenas o primeiro animal, mais velho)
+  const { name, sex, age } = species
+    .find((spe) => spe.id === ani).residents.sort((b, c) => c.age - b.age)[0];
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
