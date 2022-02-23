70db1cc06a89c2bd1e363c2311c5dcf6d1a7f5ab
lkotlarenko <lkotlarenko@protonmail.com>
2022-02-23 13:48:11 -0300

    docs: 🔥 Remove copyrighted files

--- data/zoo_data.js
+++ data/zoo_data.js
@@ -1,3 +1,5 @@
+//* This file have parts of it written by @jeanpsv
+
 const lionId = '0938aa23-f153-4937-9f88-4858b24d6bce';
 const ottersId = '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae';
 const elephantsId = 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5';
